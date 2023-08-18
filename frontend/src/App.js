587a1d8bdb81b016b68802622c51151cfdd31d8e
Laura Segouras <lausegouras@gmail.com>
2023-08-18 10:08:30 -0300

    front end setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import FileViewer from "./pages/File_viewer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/view/:id" element={<FileViewer />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
